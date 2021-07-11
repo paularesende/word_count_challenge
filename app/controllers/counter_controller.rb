54555fea6b31d1163e4455543b05b05afa8c62a7
Paula Resende <paula.resende@passwordinfo.com.br>
2021-07-11 14:39:17 -0300

    Adding characters counter

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -4,26 +4,42 @@ class CounterController < ApplicationController
   def index
     if @text_body.present?
       words_quantity = set_words_quantity
-      flash[:notice] = set_message(words_quantity)
+      char_quantity = set_characters_quantity
+      flash[:notice] = set_message(words_quantity, char_quantity)
     end
   end
 
   def set_words_quantity
-    @text_body.split.size
+    set_words.size
+  end
+
+  def set_characters_quantity
+    @text_body.gsub(/\s+/, '').length
+  end
+
+  def set_words
+    words = []
+    @text_body.split.each do |word|
+      words.push("#{word}") if word.count("a-zA-Z") > 0
+    end
+    words
   end
 
   def set_text_body
     @text_body = params[:text_body].present? ? params[:text_body][:text] : ''
   end
 
-  def set_message(text_body_size = 0)
-    return "You didn't enter any words" if text_body_size.zero?
+  def set_message(words = 0, char = 0)
+    return "You didn't enter any words" if words.zero?
 
-    text_body_size > 1 ? "You entered #{text_body_size} words" : "You entered only #{text_body_size} word"
+    words_message = words > 1 ? "You entered #{words} words. " : "You entered only #{words} word "
+    char_message = char > 1 ? "You entered #{char} characters. " : "You entered only #{char} character. "
+
+    return words_message + char_message
   end
 
 
-  # TODO: count symbols and numbers
+  # TODO: count numbers, paragraphs
   # TODO: erase message when submiting form without words
 
 end
