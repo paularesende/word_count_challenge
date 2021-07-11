2862ca6cffd190b264b5cf998ee8ee9ad2810969
Paula Resende <paula.resende@passwordinfo.com.br>
2021-07-11 13:02:41 -0300

    Encapsulating methods responsabilities

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -3,25 +3,26 @@ class CounterController < ApplicationController
 
   def index
     set_words_quantity if params[:text_body].present?
+    flash[:notice] = set_message(params[:text_body_size].to_i) if params[:text_body_size].present?
   end
 
   def set_words_quantity
     text_body_size = set_text_body(params[:text_body][:text]).split.size
-    flash[:notice] = set_message(text_body_size)
-    redirect_to counter_path(words_counted: @text_body)
+    redirect_to counter_path(words_counted: @text_body, text_body_size: text_body_size)
   end
 
-  def set_text_body(words = "")
+  def set_text_body(words = '')
     @text_body = params[:words_counted].present? ? params[:words_counted] : words
   end
 
-  def set_message(text_body_size)
-    return "You didn't enter any words" if text_body_size == 0
+  def set_message(text_body_size = 0)
+    return "You didn't enter any words" if text_body_size.zero?
+
     text_body_size > 1 ? "You entered #{text_body_size} words" : "You entered only #{text_body_size} word"
   end
 
 
-  #TODO
-  # Tratar não palavras (simbolos especiais e numeros)
-  # Apagar mensagem quando clica no botão
+  # TODO: count symbols and numbers
+  # TODO: erase message when submiting form without words
+
 end
